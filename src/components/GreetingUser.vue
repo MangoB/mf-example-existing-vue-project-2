269e941ef1fce206fd97219b1606353f41bcd225
Sinlapakorn Aunjitt <a.sinlapakorn@gmail.com>
2022-07-04 12:24:29 +0700

    Fix, indent error in GreetingUserComponent

--- src/components/GreetingUser.vue
+++ src/components/GreetingUser.vue
@@ -19,10 +19,10 @@
 </template>
 
 <script>
-  export default {
-    name: 'GreetingUserComponent',
-    data: () => ({
-      name: '',
-    }),
-  }
+export default {
+  name: 'GreetingUserComponent',
+  data: () => ({
+    name: '',
+  }),
+}
 </script>
