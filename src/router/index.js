1450267e674e00535e8f728a26c6e1835146e66f
Sinlapakorn Aunjitt <a.sinlapakorn@gmail.com>
2022-08-03 18:37:37 +0700

    Remove prefix path (from repo name) in routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/mf-root-2/',
+    path: '',
     name: 'Main page',
     component: () => import('../views/Main.vue'),
     meta: {
@@ -13,7 +13,7 @@ const routes = [
     },
   },
   {
-    path: '/mf-root-2/dog',
+    path: '/dog',
     name: 'Dog page',
     component: () => import('../views/Dog.vue'),
     meta: {
@@ -21,7 +21,7 @@ const routes = [
     },
   },
   {
-    path: '/mf-root-2/cat',
+    path: '/cat',
     name: 'Cat page',
     component: () => import('../views/Cat.vue'),
     meta: {
